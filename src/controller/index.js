ecfae3846a1c829a551f1d63f0a4787bed2de608
lizheming <i@imnerd.org>
2021-02-03 10:05:05 +0800

    fix: iframe url error

--- src/controller/index.js
+++ src/controller/index.js
@@ -2,10 +2,11 @@ const Base = require('./base.js');
 
 module.exports = class extends Base {
   async indexAction() {
+    const {url: originalUrl} = this.get();
     const url = this.ctx.href;
     const config = await this.service('wechat').getToken(url);
 
-    this.assign({url, config, host: this.ctx.state.host});
+    this.assign({url, originalUrl, host: this.ctx.state.host, config});
     return this.display();
   }
 };
